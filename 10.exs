d0bf16085d263f3794bc3595614204dcf194f3f1
Joseph Scholl <composerofsoftware@gmail.com>
2019-10-12 09:29:40 -0700

    made 10 faster and cleaner

--- 10.exs
+++ 10.exs
@@ -3,18 +3,16 @@
 defmodule Prime do
   defp is_prime(x, primes) do
     primes
-    # |> Stream.take_while(&(&1 <= :math.sqrt(x))) #TODO why does this slow it down soo much. Or when will the overhead pay off?
-    # sqrt = :math.sqrt(x) |> trunc()
-    # |> Stream.take_while(&(&1 <= sqrt))
     |> Enum.all?(&(rem(x, &1) != 0))
   end
 
   defp upto(2), do: [2]
+  defp upto(3), do: [2,3]
 
-  defp upto(x) do
+  defp upto(x) when x >= 2 do
     sqrt = :math.sqrt(x) |> trunc()
     needed = upto(sqrt)
-    needed ++ for i <- sqrt..x, is_prime(i, needed), do: i
+    needed ++ for(i <- sqrt..x, is_prime(i, needed), do: i)
   end
 
   def sumto(n), do: Enum.sum(upto(n))
